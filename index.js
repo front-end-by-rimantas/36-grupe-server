6605d89ec9dac7e770782302d8f432d2d05451f1
Rimantas <belauzas@yahoo.com>
2022-03-16 21:18:01 +0200

    Initial project file structure

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = {};
+
+app.init = () => {
+    // pasiruosti pradinius folder'ius
+
+    // pasiruosti pradinius failus
+
+    // prisijungimas prie DB (duomenu baze)
+
+    // uzkurti pati serveri (musu programa)
+    console.log('Server is running...');
+
+    // reguliariu procesu paleidimas:
+    // - istrinti senus/nebereikalingus failus
+    // - maziau naudojamu failu archivavimas
+    // - atsinaujinti informacija per/is API
+}
+
+app.init();
+
+export { app };

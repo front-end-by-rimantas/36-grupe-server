aea5b07e9636daf6c1c397633e6077e92ece278a
Rimantas <belauzas@yahoo.com>
2022-04-04 18:58:07 +0300

    More validation for service object

--- components/servicesSection.js
+++ components/servicesSection.js
@@ -1,26 +1,30 @@
 import { file } from '../lib/file.js';
 import { utils } from '../lib/utils.js';
 
+const isValidString = (text, maxSize) => {
+    if (typeof text !== 'string'
+        || text === ''
+        || text.length > maxSize) {
+        return false;
+    }
+    return true;
+}
+
 const isValidService = (service) => {
     const servicesKeysCount = 3;
+    const maxIconLength = 20;
+    const maxTitleLength = 30;
+    const maxDescLength = 100;
     if (typeof service !== 'object'
         || Array.isArray(service)
         || service === null
         || Object.keys(service).length > servicesKeysCount
-        || Object.keys(service).length < servicesKeysCount) {
+        || Object.keys(service).length < servicesKeysCount
+        || !isValidString(service.icon, maxIconLength)
+        || !isValidString(service.title, maxTitleLength)
+        || !isValidString(service.description, maxDescLength)) {
         return false;
     }
-
-    // if (typeof service !== 'object'
-    //     || Object.keys(service).length > servicesKeysCount
-    //     || typeof service.icon !== 'string'
-    //     || service.icon === ''
-    //     || typeof service.title !== 'string'
-    //     || service.title === ''
-    //     || typeof service.description !== 'string'
-    //     || service.description === '') {
-    //     return false;
-    // }
     return true;
 }
 
--- components/servicesSection.test.js
+++ components/servicesSection.test.js
@@ -26,18 +26,143 @@ describe('Service objekto dydis', () => {
         expect(isValidService({})).toBeFalsy();
     })
     test('per mazas (1)', () => {
-        expect(isValidService({ a: 1 })).toBeFalsy();
+        expect(isValidService({ icon: 'a' })).toBeFalsy();
     })
     test('per mazas (2)', () => {
-        expect(isValidService({ a: 1, b: 2 })).toBeFalsy();
+        expect(isValidService({ icon: 'a', title: 'b' })).toBeFalsy();
     })
     test('per didelis (4)', () => {
-        expect(isValidService({ a: 1, b: 2, c: 3, d: 4 })).toBeFalsy();
+        expect(isValidService({ icon: 'a', title: 'b', description: 'c', d: 4 })).toBeFalsy();
     })
     test('per didelis (5)', () => {
-        expect(isValidService({ a: 1, b: 2, c: 3, d: 4, e: 5 })).toBeFalsy();
+        expect(isValidService({ icon: 'a', title: 'b', description: 'c', d: 4, e: 5 })).toBeFalsy();
     })
     test('tinkamas dydis (3)', () => {
-        expect(isValidService({ a: 1, b: 2, c: 3 })).toBeTruthy();
+        expect(isValidService({ icon: 'a', title: 'b', description: 'c' })).toBeTruthy();
+    })
+})
+
+describe('Service icon', () => {
+    test('ne string', () => {
+        expect(isValidService({
+            icon: 1,
+            title: 'Title',
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('ne tuscias string', () => {
+        expect(isValidService({
+            icon: '',
+            title: 'Title',
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('per ilgas string', () => {
+        expect(isValidService({
+            icon: 'a'.repeat(21),
+            title: 'Title',
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('maksimalaus ilgio string', () => {
+        expect(isValidService({
+            icon: 'a'.repeat(20),
+            title: 'Title',
+            description: 'Description',
+        })).toBeTruthy();
+    })
+})
+
+describe('Service title', () => {
+    test('ne string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 1,
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('ne tuscias string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: '',
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('per ilgas string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'a'.repeat(31),
+            description: 'Description',
+        })).toBeFalsy();
+    })
+    test('maksimalaus ilgio string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'a'.repeat(30),
+            description: 'Description',
+        })).toBeTruthy();
+    })
+})
+
+describe('Service description', () => {
+    test('ne string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: 1,
+        })).toBeFalsy();
+    })
+    test('ne tuscias string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: '',
+        })).toBeFalsy();
+    })
+    test('per ilgas string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: 'a'.repeat(101),
+        })).toBeFalsy();
+    })
+    test('maksimalaus ilgio string', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: 'a'.repeat(100),
+        })).toBeTruthy();
+    })
+})
+
+describe('Service objektas su pertekline informacija', () => {
+    test('extra - per daug', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: 'Description',
+            extra: true,
+        })).toBeFalsy();
+    })
+    test('extra - per mazai', () => {
+        expect(isValidService({
+            icon: 'globe',
+            extra: true,
+        })).toBeFalsy();
+    })
+    test('extra - tinkamas kiekis', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            extra: true,
+        })).toBeFalsy();
+    })
+    test('dubliuojasi key-value', () => {
+        expect(isValidService({
+            icon: 'globe',
+            title: 'Title',
+            description: 'Description',
+            description: 'Description 2',
+        })).toBeTruthy();
     })
 })

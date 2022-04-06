46ae6f62f4edd39ed01a56cfc6c092768cfe1b18
Rimantas <belauzas@yahoo.com>
2022-04-06 21:17:57 +0300

    Token API methods

--- api/token.js
+++ api/token.js
@@ -1,13 +1,43 @@
 const handler = {};
 
-handler.token = (data) => {
+handler.token = (data, callback) => {
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
 
     if (acceptableMethods.includes(data.httpMethod)) {
-        console.log('Token: veiksmas leistinas');
-    } else {
-        console.log('Token: veiksmas NEleistinas');
+        return handler._method[data.httpMethod](data, callback);
     }
+
+    return callback(400, 'Token: veiksmas NEleistinas');
+}
+
+handler._method = {};
+
+handler._method.post = (data, callback) => {
+    return callback(200, {
+        action: 'POST',
+        msg: 'Vartotojui isduotas sesijos TOKEN',
+    })
+}
+
+handler._method.get = (data, callback) => {
+    return callback(200, {
+        action: 'GET',
+        msg: 'Visa info apie sesijos TOKEN',
+    })
+}
+
+handler._method.put = (data, callback) => {
+    return callback(200, {
+        action: 'PUT',
+        msg: 'TOKEN sekmingai atnaujintas',
+    })
+}
+
+handler._method.delete = (data, callback) => {
+    return callback(200, {
+        action: 'DELETE',
+        msg: 'TOKEN sekmingai istrintas is sistemos',
+    })
 }
 
 export default handler;

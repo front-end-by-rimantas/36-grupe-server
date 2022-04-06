48773252bfef628fc065ea27e2ebfff29e10cf76
Rimantas <belauzas@yahoo.com>
2022-04-06 21:13:39 +0300

    Account API methods

--- api/account.js
+++ api/account.js
@@ -4,14 +4,40 @@ handler.account = (data, callback) => {
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
 
     if (acceptableMethods.includes(data.httpMethod)) {
-        return callback(200, {
-            status: 'OK',
-            msg: 'Account: veiksmas leistinas',
-            time: Date.now(),
-        });
-    } else {
-        return callback(400, 'Account: veiksmas NEleistinas');
+        return handler._method[data.httpMethod](data, callback);
     }
+
+    return callback(400, 'Account: veiksmas NEleistinas');
+}
+
+handler._method = {};
+
+handler._method.post = (data, callback) => {
+    return callback(200, {
+        action: 'POST',
+        msg: 'Vartotojo paskyra sukurta sekmingai',
+    })
+}
+
+handler._method.get = (data, callback) => {
+    return callback(200, {
+        action: 'GET',
+        msg: 'Stai tau visa info apie dominanti vartotoja',
+    })
+}
+
+handler._method.put = (data, callback) => {
+    return callback(200, {
+        action: 'PUT',
+        msg: 'Vartotojo informacija sekmingai atnaujinta',
+    })
+}
+
+handler._method.delete = (data, callback) => {
+    return callback(200, {
+        action: 'DELETE',
+        msg: 'Vartotojas sekmingai istrinta is sistemos',
+    })
 }
 
 export default handler;

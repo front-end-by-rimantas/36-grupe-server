8e0059aabb72586843b8a08a2f6c900001054159
Rimantas <belauzas@yahoo.com>
2022-04-11 19:22:48 +0300

    Token API: post method full validation

--- api/token.js
+++ api/token.js
@@ -1,4 +1,6 @@
+import { file } from "../lib/file.js";
 import { IsValid } from "../lib/IsValid.js";
+import { utils } from "../lib/utils.js";
 
 const handler = {};
 
@@ -14,7 +16,7 @@ handler.token = (data, callback) => {
 
 handler._method = {};
 
-handler._method.post = (data, callback) => {
+handler._method.post = async (data, callback) => {
     const user = data.payload;
     const { email, password } = user;
     const requiredKeys = 2;
@@ -42,9 +44,43 @@ handler._method.post = (data, callback) => {
         })
     }
 
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) {
+        return callback(400, {
+            status: 'Error',
+            msg: 'Klaida bandant prisijungti: nesutampa email arba slaptazodis',
+        })
+    }
+
+    const userObj = utils.parseJSONtoObject(readMsg);
+    if (!userObj) {
+        return callback(500, {
+            status: 'Error',
+            msg: 'Klaida bandant nuskaityti vartotojo duomenis',
+        })
+    }
+
+    const hashedPassword = utils.hash(password);
+    if (hashedPassword !== userObj.password) {
+        return callback(400, {
+            status: 'Error',
+            msg: 'Klaida bandant prisijungti: nesutampa email arba slaptazodis',
+        })
+    }
+
+    const token = {};
+
+    const [createErr] = await file.create('token', 'token.json', token);
+    if (createErr) {
+        return callback(500, {
+            status: 'Error',
+            msg: 'Klaida bandant isduoti sesijos Token',
+        })
+    }
+
     return callback(200, {
-        action: 'POST',
-        msg: 'Vartotojui isduotas sesijos TOKEN',
+        status: 'Success',
+        msg: token,
     })
 }
 

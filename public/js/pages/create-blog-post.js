9afcc21e865160a0dcdd30bca67f9c3e42951069
Rimantas <belauzas@yahoo.com>
2022-04-21 20:53:49 +0300

    Create blog post page JS file

---
+++ public/js/pages/create-blog-post.js
@@ -0,0 +1,45 @@
+import { IsValid } from '../components/is-valid/IsValid.js';
+
+const formDOM = document.querySelector('.form');
+const errorsDOM = formDOM.querySelector('.form-errors');
+const allInputsDOM = formDOM.querySelectorAll('input');
+const submitDOM = formDOM.querySelector('button');
+
+submitDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const errors = [];
+    const formData = {};
+    for (const inputDOM of allInputsDOM) {
+        const { id, value, dataset } = inputDOM;
+        const validationRule = IsValid[dataset.validation];
+        const [err, status] = validationRule(value);
+        if (err) {
+            errors.push(status);
+        }
+        formData[id] = value;
+    }
+
+    errorsDOM.innerText = errors.join('\r\n');
+
+    if (errors.length === 0) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                const data = JSON.parse(this.responseText);
+
+                if (data.status === 'Success') {
+                    if (data.action.type === 'redirect') {
+                        location.href = data.action.href;
+                    }
+                }
+
+                if (data.status === 'Error') {
+                    errorsDOM.innerText = data.msg;
+                }
+            }
+        };
+        xhttp.open("POST", formDOM.action, true);
+        xhttp.send(JSON.stringify(formData));
+    }
+})

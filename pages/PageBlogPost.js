0ff5df99e3ee9ff1297a597a8b66c5f250c1427a
Rimantas <belauzas@yahoo.com>
2022-04-26 20:14:52 +0300

    Post inner page

--- pages/PageBlogPost.js
+++ pages/PageBlogPost.js
@@ -1,4 +1,6 @@
+import { file } from "../lib/file.js";
 import { PageTemplate } from "../lib/PageTemplate.js";
+import { utils } from "../lib/utils.js";
 
 class PageBlogPost extends PageTemplate {
     /**
@@ -11,11 +13,46 @@ class PageBlogPost extends PageTemplate {
         this.pageCSSfileName = 'blog-post';
     }
 
-    getPostData() {
-        return {};
+    async getPostData() {
+        const routeParts = this.data.trimmedPath.split('/');
+        if (routeParts.length !== 2) {
+            return {};
+        }
+
+        const [readErr, readContent] = await file.read('blog', routeParts[1] + '.json');
+        if (readErr) {
+            return {};
+        }
+
+        const postObj = utils.parseJSONtoObject(readContent);
+        if (!postObj) {
+            return {};
+        }
+
+        const [authReadErr, authReadContent] = await file.read('accounts', postObj.author + '.json');
+        if (authReadErr) {
+            postObj.authorName = 'AnonyMouse';
+        }
+
+        const authorObj = utils.parseJSONtoObject(authReadContent);
+        postObj.authorName = authorObj ? authorObj.username : 'AnonyMouse';
+
+        return postObj;
     }
 
-    isValidPost() {
+    isValidPost(post) {
+        if (typeof post !== 'object'
+            || post === null
+            || Array.isArray(post)
+            || typeof post.title !== 'string'
+            || post.title === ''
+            || typeof post.content !== 'string'
+            || post.content === ''
+            || typeof post.authorName !== 'string'
+            || post.authorName === '') {
+            return false;
+        }
+
         return true;
     }
 
@@ -30,12 +67,12 @@ class PageBlogPost extends PageTemplate {
         return `<section class="container blog-inner">
                     <h1 class="row title">${post.title}</h1>
                     <p class="row">${post.content}</p>
-                    <footer class="row">Author</footer>
+                    <footer class="row">${post.authorName}</footer>
                 </section>`;
     }
 
-    mainHTML() {
-        const postData = this.getPostData();
+    async mainHTML() {
+        const postData = await this.getPostData();
         if (this.isValidPost(postData)) {
             return this.correctPostHTML(postData);
         } else {
